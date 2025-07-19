660e53b560981330dd7934b43255ecf093d12cae
Hiroyuki Ikuno <sam2kaikaramegusuri@gmail.com>
2025-07-20 01:06:42 +0900

    update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,54 +1,53 @@
 #!/bin/bash
 
-BUILD_DIR=pi-build
-BBLAYERS_CONF=conf/bblayers.conf
-LOCAL_CONF=conf/local.conf
-MACHINE=raspberrypi3-64
-
 WORKSPACE=~/shared
 
-IMGDIR=$WORKSPACE/$BUILD_DIR/tmp/deploy/images/$MACHINE
+BUILD_DIR=~/pi-build
+LAYERS_DIR=$WORKSPACE/layers
+
+BBLAYERS_CONF=$BUILD_DIR/conf/bblayers.conf
+LOCAL_CONF=$BUILD_DIR/conf/local.conf
+
+MACHINE=raspberrypi3-64
+
+
+IMGDIR=$BUILD_DIR/tmp/deploy/images/$MACHINE
 
 if [ -d $BUILD_DIR ]; then
-  rm $BUILD_DIR/$BBLAYERS_CONF $BUILD_DIR/$LOCAL_CONF
+  rm $BBLAYERS_CONF $LOCAL_CONF
 fi
 
-source poky/oe-init-build-env $BUILD_DIR
+source $LAYERS_DIR/poky/oe-init-build-env $BUILD_DIR
 
 bitbake-layers add-layer \
-  ../meta-openembedded/meta-oe \
-  ../meta-raspberrypi \
-  ../meta-customize
+  $LAYERS_DIR/meta-webkit \
+  $LAYERS_DIR/meta-openembedded/meta-oe \
+  $LAYERS_DIR/meta-openembedded/meta-python \
+  $LAYERS_DIR/meta-openembedded/meta-multimedia \
+  $LAYERS_DIR/meta-raspberrypi \
 
 cat <<EOF >> $LOCAL_CONF
+
 MACHINE = "$MACHINE"
 
 # for u-boot
 RPI_USE_U_BOOT = "1"
-# for serial console. appear /dev/ttyS0
-ENABLE_UART = "1"
 
 # for systemd
 INIT_MANAGER = "systemd"
 
-IMAGE_INSTALL:append = " testapp libexample"
-# for ssh
-IMAGE_INSTALL:append = " openssh ssh-pregen-hostkeys avahi-daemon"
-# for gdbserver
-IMAGE_INSTALL:append = " gdbserver"
-INHIBIT_PACKAGE_STRIP = "1"
-# for nfs boot
-# CMDLINE_ROOTFS = "ip=dhcp root=/dev/nfs nfsroot=192.168.1.29:/,vers=4,tcp rw rootfstype=ext4 rootwait"
-
+# for wpe
+MACHINE_FEATURES:append = " vc4graphics"
+GPU_MEM_256 = "128"
+GPU_MEM_512 = "196"
+GPU_MEM_1024 = "396"
+DISTRO_FEATURES:append = " opengl egl wayland pam"
+IMAGE_FEATURES:append = " ssh-server-dropbear hwcodecs"
+PREFERRED_PROVIDER_virtual/wpebackend = "wpebackend-fdo"
+IMAGE_INSTALL:append = " wpewebkit cog"
+LICENSE_FLAGS_ACCEPTED += "synaptics-killswitch"
 EOF
 
-
-function cpimg() {
-  DSTDIR=/mnt
-  sudo cp $IMGDIR/core-image-minimal-$MACHINE.rootfs.wic.bz2 $DSTDIR
-  sudo cp $IMGDIR/core-image-minimal-$MACHINE.rootfs.wic.bmap $DSTDIR
-}
-
 i=$IMGDIR
-b=$WORKSPACE/$BUILD_DIR
-r=$WORKSPACE/$BUILD_DIR/tmp/work/${MACHINE/-/_}-poky-linux/core-image-minimal/1.0/rootfs
+b=$BUILD_DIR
+r=$BUILD_DIR/tmp/work/${MACHINE/-/_}-poky-linux/core-image-minimal/1.0/rootfs
